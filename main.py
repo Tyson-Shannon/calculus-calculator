5cf9772888fd0a274c1d532e5e2a3102627526fc
Tyson Shannon <129625009+Tyson-Shannon@users.noreply.github.com>
2023-12-07 10:01:58 -0600

    Update main.py
    added support for pi values. Added save and clear buttons to delete data or save it to newly created file.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ pip install:
 sympy
 '''
 import tkinter as tk
+import datetime
+import math
 import calculations
 
 #get question
@@ -24,6 +26,7 @@ def enter():
 def getAnswer(question):
     #replaces math calls with valid python values
     question = question.replace("inf", "float('inf')")
+    question = question.replace("pi", "math.pi")
     question = question.replace("[", "(")
     question = question.replace("]", ")")
     #splices in extra info for function to run
@@ -54,7 +57,7 @@ def cPLZ():
     cWindow = tk.Tk()
     cWindow.title("C's Get Degrees")
     cWindow["bg"] = "goldenrod"
-    cWindow.geometry("500x500")
+    cWindow.geometry("550x550")
     
     cLabel1 = tk.Label(cWindow, text="What do you need to get a C?", fg="purple1", bg="goldenrod", font=("Times", "24", "bold italic"))
     cLabel1.pack()
@@ -85,22 +88,49 @@ def help():
     helpWindow = tk.Tk()
     helpWindow.title("Help")
     helpWindow["bg"] = "goldenrod"
-    helpWindow.geometry("500x500")
+    helpWindow.geometry("550x550")
     frame2 = tk.Frame(helpWindow, bg="goldenrod")
     frame2.pack()
-    limitL = tk.Label(frame2, text="Limit:\nlimit['y', 'x', v]\ny= function\nx= symbol being limitted\nv= value symbol is going towards (inf for infinity)", bg="goldenrod", justify=tk.LEFT)
+    limitL = tk.Label(frame2, text="Limit:\nlimit['f', 'x', v]\nf= function\nx= symbol being limitted\nv= value symbol is going towards", bg="goldenrod", justify=tk.LEFT)
     limitL.grid(column=0, row=0)
-    derL = tk.Label(frame2, text="Derivative:\nder['y', 'x']\ny= function\nx= symbol being derived", bg="goldenrod", justify=tk.LEFT)
+    derL = tk.Label(frame2, text="Derivative:\nder['f', 'x']\nf= function\nx= symbol being derived", bg="goldenrod", justify=tk.LEFT)
     derL.grid(column=1, row=0)
-    difL = tk.Label(frame2, text="Differential:\ndif['y', 'x']\ny= function\nx= symbol being differentiated", bg="goldenrod", justify=tk.LEFT)
+    difL = tk.Label(frame2, text="Differential:\ndif['f', 'x']\nf= function\nx= symbol being differentiated", bg="goldenrod", justify=tk.LEFT)
     difL.grid(column=0, row=1)
+    int1L = tk.Label(frame2, text="Indefinite Integral:\nintegrate1['f', 'x']\nf= function\nx= symbol being integrated", bg="goldenrod", justify=tk.LEFT)
+    int1L.grid(column=1, row=1)
+    int2L = tk.Label(frame2, text="Definite Integral:\nintegrate2['f', 'x', l, u]\nf= function\nx= symbol being integrated\nl= lower limit\nu= upper limit", bg="goldenrod", justify=tk.LEFT)
+    int2L.grid(column=0, row=2)
+    washL = tk.Label(frame2, text="Washer Method:\nwasher['f', 'g', 'x', l, u]\nf= function 1\ng= function 2\nx= symbol being integrated\nl= lower limit\nu= upper limit", bg="goldenrod", justify=tk.LEFT)
+    washL.grid(column=1, row=2)
+    explain = tk.Label(helpWindow, text="inf for infinity\npi for pi\nBoth [] and () will work\nQuote non-numerical values such as variables and functions ie. 'x'", bg="yellow")
+    explain.pack()
     helpWindow.mainloop()
+
+#save historyBox
+def save():
+    #get data from history text box
+    data = historyBox.get("1.0","end-1c")
+    #create file useing date and time for unique name
+    now = datetime.datetime.now()
+    fileName = now.strftime("%Y%m%d%H%M%S")+".txt"
+    #store data in file making sure it is not overwriting previously created file
+    try:
+        newFile = open(fileName, "x")
+        newFile.write(data)
+        newFile.close()
+    except:
+        historyBox.insert(tk.END,"Could not save.\n")
+
+#clear historyBox
+def clear():
+    historyBox.delete("1.0","end-1c")
 
 #GUI for calculator (main gui)
 window = tk.Tk()
 window.title("Calc Calculator")
 window["bg"] = "goldenrod"
-window.geometry("500x500")
+window.geometry("550x550")
 
 
 title = tk.Label(window, text="Code to Help You Cheat in Calculus", fg="purple1", bg="goldenrod", font=("Times", "24", "bold italic"))
@@ -115,16 +145,21 @@ space1.pack()
 frame1 = tk.Frame(window, bg="goldenrod")
 frame1.pack()
 
-mathInput = tk.Entry(frame1)
+mathInput = tk.Entry(frame1, width=25)
 mathInput.grid(column=0, row=0)
 enterBut = tk.Button(frame1, text="Enter", command=lambda:enter())
 enterBut.grid(column=1, row=0)
 
-cBut = tk.Button(frame1, text="Minimum Grade to Get a C", command=lambda:cPLZ())
+cBut = tk.Button(frame1, text="Minimum Grade to Get a C", width = 20, command=lambda:cPLZ())
 cBut.grid(column=0, row=1)
 
-helpBut = tk.Button(frame1, text="Help", command=lambda:help())
+helpBut = tk.Button(frame1, text="Help", width = 20, command=lambda:help())
 helpBut.grid(column=0, row=2)
+
+saveBut = tk.Button(frame1, text="Save", width = 20, command=lambda:save())
+clearBut = tk.Button(frame1, text="Clear", width = 20, command=lambda:clear())
+saveBut.grid(column=0, row=3)
+clearBut.grid(column=0, row=4)
 
 
 window.mainloop()
