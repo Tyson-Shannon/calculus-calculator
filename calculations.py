8af5b3e0364733dd8627bb82ebeb3e2abd193092
Tyson Shannon <129625009+Tyson-Shannon@users.noreply.github.com>
2023-12-07 10:00:13 -0600

    Update calculations.py
    added both type of integrations and washer method. Supplied support for math library incase pi value is submitted

--- calculations.py
+++ calculations.py
@@ -1,6 +1,7 @@
 #Math calculations and output
 #pip install sympy
 import sympy
+import math
 
 #takes function y and sets limit for symbol to value
 def limit(y, symbol, value, historyBox, tk):
@@ -16,3 +17,19 @@ def der(f, symbol, historyBox, tk):
 def dif(f, symbol, historyBox, tk):
     x = sympy.Symbol(symbol)
     historyBox.insert(tk.END, "A: "+str(sympy.diff(f, x))+"\n")
+
+#indefinite integrate a function with respect to symbol
+def integrate1(f, symbol, historyBox, tk):
+    x = sympy.Symbol(symbol)
+    historyBox.insert(tk.END, "A: "+str(sympy.integrate(f, x))+"\n")
+
+#definite integrate a function with respect to symbole with lower limit and upper limit
+def integrate2(f, symbol, lower, upper, historyBox, tk):
+    x = sympy.Symbol(symbol)
+    historyBox.insert(tk.END, "A: "+str(sympy.integrate(f, (x, lower, upper)))+"\n")
+
+#washer is integrate(a, b) pi(f^2-g^2) 
+def washer(f, g, symbol, lower, upper, historyBox, tk):
+    x = sympy.Symbol(symbol)
+    expr = "("+f+")**2-("+g+")**2"
+    historyBox.insert(tk.END, "A: PI*"+str(sympy.integrate(expr, (x, lower, upper)))+"\n")
