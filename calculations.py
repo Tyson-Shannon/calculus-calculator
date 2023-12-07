7c2960247c53c51446c13fb25aa50e33da0c3ef7
Tyson Shannon <129625009+Tyson-Shannon@users.noreply.github.com>
2023-12-07 16:32:31 -0600

    Update calculations.py
    added disc methods, Riemann sums, sequences and series, and a still buggy arc length function

--- calculations.py
+++ calculations.py
@@ -33,3 +33,75 @@ def washer(f, g, symbol, lower, upper, historyBox, tk):
     x = sympy.Symbol(symbol)
     expr = "("+f+")**2-("+g+")**2"
     historyBox.insert(tk.END, "A: PI*"+str(sympy.integrate(expr, (x, lower, upper)))+"\n")
+
+#discx is integrate(a, b) pi(f^2)
+def discx(f, symbol, lower, upper, historyBox, tk):
+    x = sympy.Symbol(symbol)
+    expr = "("+f+")**2"
+    historyBox.insert(tk.END, "A: PI*"+str(sympy.integrate(expr, (x, lower, upper)))+"\n")
+
+#discy is integrate(a, b) pi(y)
+def discy(f, symbol, lower, upper, historyBox, tk):
+    y = sympy.Symbol(symbol)
+    historyBox.insert(tk.END, "A: PI*"+str(sympy.integrate(f, (y, lower, upper)))+"\n")
+
+#riemann sums left, middle, and right where n=# of slices on interval [a, b]
+def riemannl(f, symbol, a, b, n, historyBox, tk):
+    delta = (b-a)/n
+    i = a
+    rSum = 0
+    while(i<b):
+        xi = f.replace(symbol, str(i))
+        rSum = rSum+(eval(xi)*delta)
+        i = i+delta
+    historyBox.insert(tk.END, "A: "+str(rSum)+"\n")
+
+def riemannm(f, symbol, a, b, n, historyBox, tk):
+    delta = (b-a)/n
+    i = a
+    rSum = 0
+    while(i<b):
+        x1 = eval(f.replace(symbol, str(i)))
+        x2 = eval(f.replace(symbol, str(i+1)))
+        eq = (x1+x2)/2
+        rSum = rSum+(eq*delta)
+        i = i+delta
+    historyBox.insert(tk.END, "A: "+str(rSum)+"\n")
+
+def riemannr(f, symbol, a, b, n, historyBox, tk):
+    delta = (b-a)/n
+    i = a
+    rSum = 0
+    while(i<b):
+        xi = f.replace(symbol, str(i+1))
+        rSum = rSum+(eval(xi)*delta)
+        i = i+delta
+    historyBox.insert(tk.END, "A: "+str(rSum)+"\n")
+
+#sequences convergent or divergent test where n=starting point
+def seq(f, symbol, n, historyBox, tk):
+    x = sympy.Symbol(symbol)
+    seqLimit = sympy.limit(f, x, float("inf"))
+    if (seqLimit == float("inf") or seqLimit == -float("inf")):
+        historyBox.insert(tk.END, "A: Diverges to "+str(seqLimit)+"\n")
+    elif (seqLimit == sympy.nan):
+        historyBox.insert(tk.END, "A: Divergent, limit DNE\n")
+    else:
+        historyBox.insert(tk.END, "A: Converges to "+str(seqLimit)+"\n")
+    
+#series convergent or divergent improper integral test where n=starting point
+def series(f, symbol, n, historyBox, tk):
+    x = sympy.Symbol(symbol)
+    serInt = sympy.integrate(f, (x, n, float("inf")))
+    if (serInt == float("inf") or serInt == -float("inf")):
+        historyBox.insert(tk.END, "A: Diverges to "+str(serInt)+"\n")
+    elif (serInt == sympy.nan):
+        historyBox.insert(tk.END, "A: Divergent, integral DNE\n")
+    else:
+        historyBox.insert(tk.END, "A: Converges to "+str(serInt)+"\n")
+
+#arc length is integrate(a, b) sqrt(1+(f')**2)
+def arcl(fPrime, symbol, lower, upper, historyBox, tk):
+    x = sympy.Symbol(symbol)
+    expr = "(1+("+fPrime+")**2)**0.5"
+    historyBox.insert(tk.END, "A: "+str(sympy.integrate(expr, (x, lower, upper)))+"\n")
